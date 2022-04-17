73cd21e9feac9413f1682b607d79337bbf6137ef
Gonzalo Martinez <gon.martinez@icloud.com>
2022-04-17 02:18:07 -0300

    Renamed player input script to avoid overrides

--- Assets/Scripts/PlayerInputActions.cs
+++ Assets/Scripts/PlayerInputActions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerInput : MonoBehaviour
+public class PlayerInputActions : MonoBehaviour
 {
     [SerializeField] PlayerMovement movement;
     bool pressingDown = false;
